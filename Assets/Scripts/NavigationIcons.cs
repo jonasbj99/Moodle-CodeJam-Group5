f42e4c4a573420f158a2f751f0b45c085b1e8efd
jonasbj99 <99955044+jonasbj99@users.noreply.github.com>
2022-04-07 11:15:33 +0200

    nyt

---
+++ Assets/Scripts/NavigationIcons.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationIcons : MonoBehaviour
+{
+    [SerializeField] GameObject markedSprite;
+    [SerializeField] GameObject targetPanel;
+
+    public void Update()
+    {
+        if (targetPanel.active)
+        {
+            markedSprite.SetActive(true);
+        }
+        else
+        {
+            markedSprite.SetActive(false);
+        }
+    }
+}
