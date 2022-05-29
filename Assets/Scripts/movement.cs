8dc58f3e747e4fd49ef5f02eb043e918f216742d
soiejn <94910957+soiejn@users.noreply.github.com>
2022-05-29 15:42:45 +0200

    comments :P

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -8,6 +8,9 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    // reference til rigidbody, retningen bølgerne bevæger sig (på x aksen),
+    // og hastigheden bølgerne bevæger sig, med variable
+    //BEMÆRK!! rigidbody er 2D, da den kun skal bevæge sig på en 2D plan
     Rigidbody2D rb;
     float dirx;
     public float moveSpeed = 80f;
@@ -16,12 +19,15 @@ public class movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //giver rigidbody en component
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // value fra accelerometer input på x aksen * hastigheden
+        // linje 31 viser indenfor hvilken ramme bølgerne kan bevæge sig, så bølgerne ikke bevæger sig uendeligt
         dirx = Input.acceleration.x * moveSpeed;
         transform.position = new Vector2(Mathf.Clamp(transform.position.x, 400f, 600f), transform.position.y);
     }
