806678b5ebd8c49fcaa26914baf179c6ab2e2bd7
jonasbj99 <99955044+jonasbj99@users.noreply.github.com>
2022-05-31 14:31:18 +0200

    Added comments
    NavigationBar and NavigationIcons

--- Assets/Scripts/NavigationBar.cs
+++ Assets/Scripts/NavigationBar.cs
@@ -6,37 +6,10 @@ using UnityEngine;
 // https://www.youtube.com/watch?v=Rfv5M14ZoGY&ab_channel=InfoGamer
 public class NavigationBar : MonoBehaviour
 {
-    /*
-    public GameObject coursePanel;
-    public GameObject calenderPanel;
-    public GameObject menuPanel;
-    */
-
+    // Array of GameObjects called panels
     [SerializeField] GameObject[] panels;
 
-    /*
-    public void CoursePage()
-    {
-        coursePanel.SetActive(true);
-        calenderPanel.SetActive(false);
-        menuPanel.SetActive(false);
-    }
-
-    public void CalenderPage()
-    {
-        coursePanel.SetActive(false);
-        calenderPanel.SetActive(true);
-        menuPanel.SetActive(false);
-    }
-
-    public void MenuPage()
-    {
-        coursePanel.SetActive(false);
-        calenderPanel.SetActive(false);
-        menuPanel.SetActive(true);
-    }
-    */
-
+    // Method with for-loop to deactivate every panel and reactivate activePanel
     public void NavigationBarClick(GameObject activePanel)
     {
         for (int i = 0; i < panels.Length; i++)
